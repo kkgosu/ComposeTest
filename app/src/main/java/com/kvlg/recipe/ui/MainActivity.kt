edd0d64284e2b8d37e099ba5856f11261ae892aa
kkgosu <kostya96.08@mail.ru>
2021-06-20 23:45:02 +0300

    Row Layout

--- app/src/main/java/com/kvlg/recipe/ui/MainActivity.kt
+++ app/src/main/java/com/kvlg/recipe/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +15,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -47,11 +51,24 @@ class MainActivity : ComponentActivity() {
                     contentScale = ContentScale.Crop
                 )
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Happy meal", style = TextStyle(fontSize = TextUnit(24f, TextUnitType.Sp)))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "Happy meal", style = TextStyle(fontSize = TextUnit(24f, TextUnitType.Sp)))
+                        Text(
+                            text = "$5.99",
+                            style = TextStyle(fontSize = TextUnit(20f, TextUnitType.Sp)),
+                            color = Color(0xFF85bb65),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
                     Spacer(modifier = Modifier.padding(top = 10.dp))
                     Text(text = "800 calories", style = TextStyle(fontSize = TextUnit(20f, TextUnitType.Sp)))
                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "$5.99", style = TextStyle(fontSize = TextUnit(20f, TextUnitType.Sp)), color = Color(0xFF85bb65))
+                    Button(onClick = {}, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(text = "Order Now!")
+                    }
                 }
             }
         }
