5920e98db664dc83782a83022821896f0701b6ca
kkgosu <kostya96.08@mail.ru>
2021-06-30 20:41:37 +0300

    Constraint Layout with Jetpack Compose

--- app/src/main/java/com/kvlg/recipe/ui/components/CircularIndeterminateProgressBar.kt
+++ app/src/main/java/com/kvlg/recipe/ui/components/CircularIndeterminateProgressBar.kt
@@ -1,14 +1,19 @@
 package com.kvlg.recipe.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 
 /**
  * @author Konstantin Koval
@@ -19,14 +24,40 @@ fun CircularIndeterminateProgressBar(
     isDisplayed: Boolean
 ) {
     if (isDisplayed) {
-        Row(
-            modifier = Modifier
-                .padding(20.dp)
-                .fillMaxWidth(), horizontalArrangement = Arrangement.Center
-        ) {
-            CircularProgressIndicator(
-                color = MaterialTheme.colors.primary
-            )
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            val constraints = if (minWidth < 600.dp) {
+                decoupledConstraints(0.3f)
+            } else {
+                decoupledConstraints(0.7f)
+            }
+
+            ConstraintLayout(modifier = Modifier.fillMaxSize(), constraintSet = constraints) {
+                CircularProgressIndicator(
+                    modifier = Modifier.layoutId("progressBar"),
+                    color = MaterialTheme.colors.primary
+                )
+                Text(text = "Loading...", style = TextStyle(color = Color.Black, fontSize = 16.sp), modifier = Modifier.layoutId("text"))
+            }
+        }
+    }
+}
+
+private fun decoupledConstraints(verticalBias: Float): ConstraintSet {
+    return ConstraintSet {
+        val guideline = createGuidelineFromTop(verticalBias)
+        val progressBar = createRefFor("progressBar")
+        val text = createRefFor("text")
+
+        constrain(progressBar) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(text) {
+            top.linkTo(progressBar.bottom)
+            start.linkTo(progressBar.start)
+            end.linkTo(progressBar.end)
         }
     }
 }
