17ac1b97c5552a3e677b582ad0e23a8cf78d2f28
kkgosu <kostya96.08@mail.ru>
2021-07-11 13:23:07 +0300

    Scaffold and Slot APIs with Jetpack Compose

--- app/src/main/java/com/kvlg/recipe/ui/RecipeListFragment.kt
+++ app/src/main/java/com/kvlg/recipe/ui/RecipeListFragment.kt
@@ -2,15 +2,14 @@ package com.kvlg.recipe.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.kvlg.recipe.RecipeApplication
 import com.kvlg.recipe.RecipeApplication.Companion.toggleLightTheme
 import com.kvlg.recipe.ui.components.CircularIndeterminateProgressBar
 import com.kvlg.recipe.ui.components.LoadingRecipeListShimmer
@@ -24,20 +23,21 @@ import com.kvlg.recipe.ui.components.SearchAppBar
 
 @Composable
 fun RecipeListFragment(viewModel: RecipeViewModel, onRecipeClick: (Long) -> Unit) {
-    Column(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
-        SearchAppBar(viewModel = viewModel, onToggleTheme = { toggleLightTheme() })
-        if (viewModel.loading.value) {
-            LoadingRecipeListShimmer(imageHeight = 260.dp)
-        }
-/*        Row(
+    Scaffold(
+        topBar = {
+            SearchAppBar(viewModel = viewModel, onToggleTheme = { toggleLightTheme() })
+        },
+        bottomBar = {},
+        drawerContent = {})
+    {
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp), horizontalArrangement = Arrangement.Center
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.background)
         ) {
-            val state = remember { mutableStateOf(IDLE) }
-            AnimatedHeartButton(modifier = Modifier, buttonState = state, onToggle = { state.value = if (state.value == IDLE) ACTIVE else IDLE })
-        }*/
-        Box(modifier = Modifier.fillMaxSize()) {
+            if (viewModel.loading.value) {
+                LoadingRecipeListShimmer(imageHeight = 260.dp)
+            }
             LazyColumn {
                 itemsIndexed(
                     items = viewModel.recipes.value
@@ -49,4 +49,12 @@ fun RecipeListFragment(viewModel: RecipeViewModel, onRecipeClick: (Long) -> Unit
             CircularIndeterminateProgressBar(isDisplayed = viewModel.loading.value)
         }
     }
+/*        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp), horizontalArrangement = Arrangement.Center
+        ) {
+            val state = remember { mutableStateOf(IDLE) }
+            AnimatedHeartButton(modifier = Modifier, buttonState = state, onToggle = { state.value = if (state.value == IDLE) ACTIVE else IDLE })
+        }*/
 }
